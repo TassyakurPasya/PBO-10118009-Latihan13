e50f5aab8352db5305c7098b0c3d9e8152587d6b
TassyakurPasya <55564127+TassyakurPasya@users.noreply.github.com>
2019-09-25 18:36:15 +0700

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package relational;
+
+/**
+ *
+ * @author Lenovo
+ * Nama : Tassyakur Pasya
+ * Kelas : IF-01
+ * NIM : 10118009
+ * Matkul : PBO-01
+ * Deskripsi Program : Relational
+ */
+public class Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("a >= b = " + (bil2 >= bil1));
+        System.out.println("a <= b = " + (bil2 <= bil1));
+    }
+    
+}
